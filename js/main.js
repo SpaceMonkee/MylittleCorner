b4426cc085e7a6a62d7e63ff6851ead6ac2b7aa9
SpaceMonkee <joshthebro4@gmail.com>
2017-11-21 14:57:26 +0200

    v8

--- js/main.js
+++ js/main.js
@@ -1,83 +1,16 @@
-// Initial scroll position
-var scrollState = 0;
+//carousel for mobile
 
-// Store navbar classes
-var navClasses = document.getElementById('navBar').classList;
+var myIndex = 0;
+carousel();
 
-// returns current scroll position
-var scrollTop = function() {
-  return window.scrollY;
-};
-
-// Primary scroll event function
-var scrollDetect = function(home, down, up) {
-  // Current scroll position
-  var currentScroll = scrollTop();
-  if (scrollTop() === 0) {
-    home();
-  } else if (currentScroll > scrollState) {
-    down();
-  } else {
-    up();
-  }
-  // Set previous scroll position
-  scrollState = scrollTop();
-};
-
-function homeAction() {
-  console.log("home");
-}
-
-function downAction() {
-  navClasses.remove('open');
-  navClasses.add('collapse');
-}
-
-function upAction() {
-  navClasses.remove('collapse');
-  navClasses.add('open');
-}
-
-window.addEventListener("scroll", function() {
-  scrollDetect(homeAction, downAction, upAction);
-});
-
-//My images
-
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
-}
-
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
-
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
-
-  }
-  x[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " w3-opacity-off";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
 }
