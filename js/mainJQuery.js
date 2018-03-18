d0436b3e0e6016dc4a5dccc5e4d9e992e235ebe4
SpaceMonkee <joshthebro4@gmail.com>
2018-03-18 18:50:41 +0200

    final update for now

--- js/mainJQuery.js
+++ js/mainJQuery.js
@@ -1,7 +1,7 @@
 //Modal for Desktop
 
-$("#modal-launcher").lightGallery({
-    selector: '.work',
-    thumbnail:true,
-});
-    
+$(document).ready(function() {
+    $('#interest').lightGallery({
+        thumbnail:true
+    });
+});  
